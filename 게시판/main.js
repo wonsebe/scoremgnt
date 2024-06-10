04184f877d7c06db128b360280b8579900f170ba
Kimviruscool <sinsa9122@naver.com>
2024-06-10 18:00:03 +0900

    0610 1800 LOGIN-MAIN연결

--- 게시판/main.js
+++ 게시판/main.js
@@ -2,6 +2,13 @@ let boardlist = [
     {no : 1,title : 'TEST입니다.',writer : '본인',date : '2024-6-08', view : '5'  }
 ]
 
+//localstorage 에서 studentsNum 받아와서 
+//localstorage 에서 studentsName 찾아야함
+
+let memberList = []
+
+
+
 printboard();
 printboard1();
 function printboard() {
@@ -30,6 +37,7 @@ function printboard1() {
     
     //어디에
     let board1 = document.querySelector('#board1');
+
     //무엇을
     let html = ``;
     for(let i = 0 ; i < boardlist.length ; i++){
@@ -39,9 +47,15 @@ function printboard1() {
                  <th> ${boardlist[i].writer} </th>
                  <tr>`
     }
-    
+
 
 
     //출력
     board1.innerHTML = html;
+}
+
+function 검사(){
+    let 검사 = sessionStorage.getItem('studentNum')
+
+    if(){}else{login();}
 }
