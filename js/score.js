ba89249e74bf400f8e77c76c72191517aa012808
syeon8344 <yeon8344@gmail.com>
2024-06-10 18:00:36 +0900

    0610 6:00PM

--- js/score.js
+++ js/score.js
@@ -23,41 +23,52 @@ let score = [
 
 let student = [
     {studentNum : 1, studentName : "aaa", studentCode : 10101, contact : 123-456-7890, pw : 1234},
-    {studentNum : 2, studentName : "bbb", studentCode : 10101, contact : 123-456-7890, pw : 1234},
-    {studentNum : 3, studentName : "ccc", studentCode : 10101, contact : 123-456-7890, pw : 1234},
-    {studentNum : 4, studentName : "ddd", studentCode : 10101, contact : 123-456-7890, pw : 1234}
+    {studentNum : 2, studentName : "bbb", studentCode : 10102, contact : 123-456-7890, pw : 1234},
+    {studentNum : 3, studentName : "ccc", studentCode : 10103, contact : 123-456-7890, pw : 1234},
+    {studentNum : 4, studentName : "ddd", studentCode : 10104, contact : 123-456-7890, pw : 1234}
 ]
 
 let selYear = 0; // 0 : 선택하지 않음
 let selClass = 0;
 let selSubject = 0; // 1 2 3 4 국 수 영 사회 과학
-
+let selScoreMode = 1;
+let selTestYear = 2024;
+let selTestMonth = 1;
+let isGraphDrawn = 0;
 drawChart();
 
-function drawChart(){ // 학년 반 선택 후 반 단위 점수 그래프
+function drawChart(){ // 학년 반 선택 후 *반 단위* 점수 그래프, 평균 : 전과목(사회과*2)/4 또는 과목별,
     
     if (selYear != 0 && selClass != 0 && selSubject != 0){
         let subName = subjectName();
-        console.log(`${selYear}학년 ${selClass}반 ${subName}과목 그래프`)
+        label = `${selYear}학년 ${selClass}반 ${subName}과목 그래프`
+        let studentNumList = [];
+        let y = [];
         for (s of student) {// 차트 data 구축 : 학년 반 과목을 고른 후
-            let codeData = codeSplit();
-            //if () {}
-            let x 
-
+            let code = s.studentCode;
+            let codeData = codeSplit(code);
+            if (codeData.year == selYear && codeData.class == selClass) {
+                studentNumList.push(s.studentNum);
+            }
         }
 
+        console.log(studentNumList); //필터링된 학생 번호 목록
 
+        if (isGraphDrawn == 1) {scoreChart.destroy();}
 
-        const chartHTML = document.querySelector("#scoreChart");
-            
-        new Chart(chartHTML, {
-            type: 'scatter',
+        let graphData = []; 
+        //for () {
+
+    
+        chartHTML = document.querySelector("#scoreChart");
+        scoreChart = new Chart(chartHTML, {
+            type: 'line',
             data: {
                 datasets: [{
-                    label: `${selYear}학년 ${selClass}반 ${subName}과목 그래프`,
-                    data: [], // {x: 번호(이름), y: 점수}, {...}],
-                    backgroundColor: 'rgb(255, 99, 132)'
-                  }],
+                    "label" : label,
+                    "data" : graphData, // {x: 번호(이름), y: 점수}, {...}],
+                    "backgroundColor" : 'rgb(255, 99, 132)'
+                    }],
             },
             options: {
                 scales: {
@@ -70,15 +81,14 @@ function drawChart(){ // 학년 반 선택 후 반 단위 점수 그래프
         }
         );
 
-    }
-
+        isGraphDrawn = 1;
+    };
 }
 
-
-
-function pop(){
-    console.log("pop")
+function drawTable(){
+    
 }
+
 
 function setYear(num){ // 현재 선택한 학년
     selYear = num;
@@ -98,26 +108,38 @@ function setSubject(num){ // 현재 선택한 과목
     drawChart();
 }
 
+function setScoreMode(num){ // 현재 선택한 점수분석
+    selScoreMode = num;
+    console.log("점수 분석 : "+selScoreMode)
+    drawChart();
+}
+
+function setTestYear(num){ // 현재 선택한 시험 연도
+    selTestYear = num;
+    console.log("시험 연도 : "+selTestYear)
+    drawChart();
+}
+
+function setTestMonth(num){ // 현재 선택한 시험 회차
+    selTestMonth = num;
+    console.log("시험 회차 : "+selTestMonth)
+    drawChart();
+}
+
 function subjectName(){
     switch (selSubject) {
         case 1:
-            console.log("과목 : 국어");
             return '국어';
         case 2:
-            console.log("과목 : 영어");
             return '영어';
         case 3:
-            console.log("과목 : 수학");
             return '수학';
         case 4:
             if (selClass >=1 && selClass <= 5) {
-                console.log("과목 : 사회");
                 return '사회';
             } else if (selClass >=6 && selClass <= 10) {
-                console.log("과목 : 과학");
                 return '과학';
             } else {
-                console.log("반이 선택되지 않음")
                 return 0;
             }
     }
